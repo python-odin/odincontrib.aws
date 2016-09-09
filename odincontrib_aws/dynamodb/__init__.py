c631bcf313d87a082fb3f8a6a252f19129a33049
Tim Savage <tim@savage.company>
2016-09-09 15:12:50 +1000

    Add reference to indexes

--- odincontrib_aws/dynamodb/__init__.py
+++ odincontrib_aws/dynamodb/__init__.py
@@ -1,3 +1,4 @@
 from odincontrib_aws.dynamodb.fields import *  # noqa
 from odincontrib_aws.dynamodb.table import *  # noqa
+from odincontrib_aws.dynamodb.indexes import *  # noqa
 from odincontrib_aws.dynamodb.session import Session  # noqa
