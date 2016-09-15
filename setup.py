a2cc371d513626e5775a660bf08cf75948557415
Tim Savage <tim@savage.company>
2016-09-15 12:56:15 +1000

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='odincontrib.aws',
 
-    version='0.1.3',
+    version='0.1.4',
 
     description='Odin integration with AWS',
     long_description=long_description,
