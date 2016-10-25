ce823bd0dde9a3430e4bf69b20a0f05de33202e5
Tim Savage <tim@savage.company>
2016-10-25 12:01:01 +1100

    Added create_bound_table_from_dict which binds the specified session to the resources

--- odincontrib_aws/dynamodb/utils.py
+++ odincontrib_aws/dynamodb/utils.py
@@ -1,5 +1,6 @@
 import six
 from odin.fields import Field
+from odin.resources import create_resource_from_dict
 from odin.utils import getmeta
 
 
@@ -38,3 +39,19 @@ def domino_field_iter_items(resource, fields, required_field_names=None, skip_nu
         if skip_null_values and (f.name not in required_field_names) and (value in [None, '']):
             continue
         yield f, f.prepare_dynamo(value)
+
+
+def create_bound_table_from_dict(d, table, session, full_clean=True):
+    """
+    Create a bound table from a dict. A bound table is attached to a session object.
+
+    :param d:
+    :type table: odincontrib_aws.dynamodb.Table
+    :type session: odincontrib_aws.dynamodb.Session
+    :param full_clean: Perform a full clean on the table.
+    :rtype: odincontrib_aws.dynamodb.Table
+
+    """
+    t = create_resource_from_dict(d, table, full_clean, False)
+    t.bind_to_session(session)
+    return t
